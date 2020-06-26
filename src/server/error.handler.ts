bc5cb8ea933492488c49b7f35e454e0a0bc4622c
Andre Teles <andre.telestp@gmail.com>
2020-06-25 21:05:32 -0300

    added custom validation and encrypt password

--- src/server/error.handler.ts
+++ src/server/error.handler.ts
@@ -1,19 +1,24 @@
 import { Response,  } from 'restify'
 
 export default (req: Request, res: Response, err:any, done: Function) => {
- 
+  
   err.toJSON = () => {
     return {
       message: err.message
     }
   }
 
-  switch (err.name) {
+ switch (err.name) {
     case 'MongoError':
       if(err.code === 11000) err.statusCode = 400
       break;
-      case 'ValidationError': 
+    case 'ValidationError': 
       err.statusCode = 400
+      const messages: any[] = []
+      for(let name in err.errors){
+        messages.push({message: err.errors[name].message})
+      }
+      err.toJSON = () => ({errors: messages})
       break;
   }
 
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,6 @@
 import {Schema,model,Document} from 'mongoose'
-
+import {validateCPF} from '../common/validators'
+import * as bcrypt from 'bcrypt'
 export interface User extends Document{
   name: string,
   email: string,
@@ -8,15 +9,46 @@ export interface User extends Document{
 
 const UserSchema = new Schema({
   name:{
-    type: String
+    type: String,
+    required: true,
+    maxlength:80,
+    minlength: 3
   },
   email: {
     type: String,
-    unique: true
+    unique: true, 
+    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    required: true
   },
   password: {
     type: String,
-    select: true
+    select: true,
+    required: true
+  },
+  gender:{
+    type: String,
+    required: false,
+    enum: ['Male','Female']
+  },
+  cpf: {
+    type: String,
+    required: false,
+    validate: {
+      validator: validateCPF,
+      message: '{PATH}: Invalid CPF({VALUE})'
+    }
+  }
+})
+
+UserSchema.pre('save', function(next){
+  const user: User = this
+  if(!user.isModified('password')){
+    next()
+  }else{
+    bcrypt.hash(user.password,10).then(hash => {
+      user.password = hash
+      next()
+    }).catch(next)
   }
 })
 
